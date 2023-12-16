d109afba6a009d6dea6d882707f57b9ca43fa019
mk3008 <mk3008.develop@gmail.com>
2023-12-16 13:49:04 +0900

    #3 Add TableFullName property

---
+++ test/PostgresSample/DbTableTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using RedOrb;
+using Xunit.Abstractions;
+
+namespace PostgresSample;
+
+public class DbTableTest
+{
+	public DbTableTest(ITestOutputHelper output)
+	{
+		Logger = new UnitTestLogger() { Output = output };
+	}
+
+	private readonly UnitTestLogger Logger;
+
+	[Fact]
+	public void OmitSchemaName()
+	{
+		var table = new DbTable { TableName = "test" };
+		Assert.Equal("test", table.TableName);
+		Assert.Equal("test", table.TableFullName);
+	}
+
+	[Fact]
+	public void HasSchemaName()
+	{
+		var table = new DbTable { SchemaName = "public", TableName = "test" };
+		Assert.Equal("test", table.TableName);
+		Assert.Equal("public.test", table.TableFullName);
+	}
+}
